cbd44e59d2b505f5b1772a952551a8080cf343e5
Burhan-Unal <burhanubeyan@gmail.com>
2025-11-30 18:20:36 +0300

    Revert "Attempt"

--- InvestAI/Program.cs
+++ InvestAI/Program.cs
@@ -2,6 +2,9 @@ namespace InvestAI
 {
     internal static class Program
     {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
